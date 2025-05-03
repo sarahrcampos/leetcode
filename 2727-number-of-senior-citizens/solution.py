1924e4dfdbb9b1e83020588886bcde9edb349ce6
Sarah Campos <sarahrcampos@gmail.com>
2025-05-03 23:03:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (57.41%)

---
+++ 2727-number-of-senior-citizens/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def countSeniors(self, details):
+        count = 0
+        for passenger in details:
+            age = int(passenger[11:13])
+            if age > 60:
+                count += 1
+        return count
+        
