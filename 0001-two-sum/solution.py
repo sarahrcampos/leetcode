bf23ead19739ef6b3d7605689645454e73a80cf5
Sarah Campos <sarahrcampos@gmail.com>
2025-05-11 12:41:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.4 MB (30.88%)

---
+++ 0001-two-sum/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        map = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in map:
+                return [map[complement], i]
+            map[nums[i]] = i
+
+        
