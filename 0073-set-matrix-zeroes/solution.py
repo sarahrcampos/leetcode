c0b2294acdee943994b2283a1de0ea33d41b4926
Sarah Campos <sarahrcampos@gmail.com>
2025-05-21 21:52:10 +0000

    [LeetCode Sync] Runtime - 3 ms (80.52%), Memory - 13.2 MB (41.74%)

--- 0073-set-matrix-zeroes/solution.py
+++ 0073-set-matrix-zeroes/solution.py
@@ -1,21 +1,20 @@
 class Solution(object):
-    #TODO: REFAZER AMANHÃ!!
+    #Time: O(n)
+    #Space: O(n)
     def setZeroes(self, matrix):
         ROWS, COLS = len(matrix), len(matrix[0])
-        changed = set()
+        zero_rows = set()
+        zero_cols = set()
         for i in range(ROWS):
             for j in range(COLS):
-                if (i,j) in changed:
-                    continue
                 if matrix[i][j] == 0:
-                    for x in range(ROWS):
-                        if matrix[x][j] !=0:
-                            matrix[x][j] = 0
-                            changed.add((x,j))
-                    for x in range(COLS):
-                        if matrix[i][x] !=0:
-                            matrix[i][x] = 0
-                            changed.add((i,x))
+                    zero_rows.add(i)
+                    zero_cols.add(j)
+        for i in range(ROWS):
+            for j in range(COLS):
+                if i in zero_rows or j in zero_cols:
+                    matrix[i][j] = 0
+
     
 
         
