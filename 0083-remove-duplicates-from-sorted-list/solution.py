bc1fe33dd6fa6d4f1d9a1482919963fdfbd6fb1d
Sarah Campos <sarahrcampos@gmail.com>
2025-05-25 21:26:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (38.87%)

--- 0083-remove-duplicates-from-sorted-list/solution.py
+++ 0083-remove-duplicates-from-sorted-list/solution.py
@@ -1,23 +1,23 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def deleteDuplicates(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        resultList = ListNode()
-        resultList.val = -101
-        p = resultList
-        while head:
-            temp = head.next
-            if head.val != p.val:
-                head.next = None
-                p.next = head                
-                p = p.next
-            head = temp
-        return resultList.next
-        
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        current = head
+        while current:
+            next = current.next
+            while next and next.val == current.val:
+                next = next.next
+            current.next = next
+            current = current.next
+        return head
+# 1 -> 1 -> 2
+#current = 1 [0]
+#next = 1 [1]
+#next = 2
+#current.next = 2
+
