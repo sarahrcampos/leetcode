e4d3b39e7d580ba99c2908c5534d985634d00c21
Sarah Campos <sarahrcampos@gmail.com>
2025-05-25 22:40:53 +0000

    [LeetCode Sync] Runtime - 77 ms (89.77%), Memory - 27.8 MB (69.03%)

--- 0160-intersection-of-two-linked-lists/solution.py
+++ 0160-intersection-of-two-linked-lists/solution.py
@@ -1,24 +1,21 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def getIntersectionNode(self, headA, headB):
-        """
-        :type head1, head1: ListNode
-        :rtype: ListNode
-        """
-        map = {}
-        a = headA
-        while a:
-            map[a] = True
-            a = a.next
-        b = headB
-        while b:
-            if b in map:
-                return b
-            b = b.next
-        return None
-        
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        pa, pb = headA, headB
+        while pa != pb: #não preciso verificar se pa e pb são not null, porque se ambos forem null é pq as listas são diferentes
+        #tipo, se elas não tiverem interseção, elas vão se encontrar no null (next da folha)
+            pa = pa.next if pa else headB
+            pb = pb.next if pb else headA
+        return pa
+
+# A -> a1 -> a2 -> c1 -> c2 -> c3
+# B -> b1 -> b2 -> b3 -> c1 -> c2 -> c3
+#no total, são 11 nós
+#11 = 5 + 6 = 6 + 5
+#portanto, se eu percorrer a1 inteiro (5 nós) e voltar p/ headB e percorrer tudo (6) vou acabar no 11
+#e se eu percorrer b inteiro (6 nós) e voltar p/ headA e percorrer tudo (5) vou acabar no 11 tbm
