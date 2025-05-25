63f896aac2f0276e73ea18e7c2ee84941a5f4cf3
Sarah Campos <sarahrcampos@gmail.com>
2025-05-25 19:54:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (24.00%)

--- 0392-is-subsequence/solution.py
+++ 0392-is-subsequence/solution.py
@@ -1,9 +1,10 @@
-from collections import deque
-class Solution(object):
-    def isSubsequence(self, s, t):
-        q = deque(s)
-        for c in t:
-            if q and q[0] == c:
-                q.popleft()
-        return len(q) == 0
-        
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        ps, pt = 0, 0
+        while ps < len(s):
+            while pt < len(t) and t[pt] != s[ps]:
+                pt += 1
+            if pt == len(t):
+                return False
+            pt, ps = pt + 1, ps + 1
+        return True
