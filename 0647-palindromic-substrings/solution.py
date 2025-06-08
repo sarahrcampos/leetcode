931f40108600b955d7b8825d9a65c3028447644b
Sarah Campos <sarahrcampos@gmail.com>
2025-06-08 20:53:34 +0000

    [LeetCode Sync] Runtime - 212 ms (27.20%), Memory - 25.5 MB (22.80%)

---
+++ 0647-palindromic-substrings/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        dp = [[False] * len(s) for _ in range(len(s))]
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i] == s[j] and (j-i <= 2 or dp[i+1][j-1]):
+                    dp[i][j] = True
+                    count+=1
+        return count
