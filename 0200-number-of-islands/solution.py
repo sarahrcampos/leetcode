67414e324b4df5ca042132bf089925282d2e4df3
Sarah Campos <sarahrcampos@gmail.com>
2025-06-19 18:07:00 +0000

    [LeetCode Sync] Runtime - 304 ms (12.92%), Memory - 20.2 MB (57.85%)

---
+++ 0200-number-of-islands/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        def dfs(i, j):
+            if i not in range(0,ROWS) or j not in range(0, COLS) or grid[i][j] != "1":
+                return
+            grid[i][j] = "0"
+            dfs(i-1, j)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i, j+1)
+        
+        count = 0
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1":
+                    count += 1
+                    dfs(i,j)
+        
+        return count
