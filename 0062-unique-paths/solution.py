7b428b39ad1b0111613f5591b5aa00b637b05a65
Sarah Campos <sarahrcampos@gmail.com>
2025-07-02 01:40:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (10.37%)

---
+++ 0062-unique-paths/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        counter = [[0] * (n+1) for _ in range(m+1)]
+        counter[m-1][n-1] = 1
+
+        print(counter)
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if i == m-1 and j == n-1:
+                    continue
+                counter[i][j] = counter[i+1][j] + counter[i][j+1]
+        print(counter)
+        return counter[0][0]
