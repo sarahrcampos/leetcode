15ea19c8a390e6e3ae68e6618097f8914bb5d409
Sarah Campos <sarahrcampos@gmail.com>
2025-07-05 23:53:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (14.31%)

---
+++ 1524-string-matching-in-an-array/solution.py
@@ -0,0 +1,11 @@
+#brute force solution: O(n²)
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        words.sort(key=lambda x: len(x))
+        result = []
+        for i in range(len(words)):
+            for j in range(i+1, len(words)):
+                if words[i] in words[j]:
+                    result.append(words[i])
+                    break
+        return result
