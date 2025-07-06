6ea5522a797f2a59470a5d2d1009212d265834bf
Sarah Campos <sarahrcampos@gmail.com>
2025-07-06 19:08:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (57.41%)

--- 0874-backspace-string-compare/solution.py
+++ 0874-backspace-string-compare/solution.py
@@ -1,17 +1,33 @@
-class Solution(object):
-    #Time: O(n+m)
-    #Space: O(n+m)
-    def backspaceCompare(self, s, t):
-        new_s, new_t = [], []
-        for i in range(len(s)):
-            if s[i] == "#" and new_s:
-                new_s.pop()
-            elif s[i] != "#":
-                new_s.append(s[i])
-        for j in range(len(t)):
-            if t[j] == "#" and new_t:
-                new_t.pop()
-            elif t[j] != "#":
-                new_t.append(t[j])
-        return "".join(new_t) == "".join(new_s)
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        p1, p2 = len(s)-1, len(t)-1
+        while p1 >= 0 or p2 >= 0:
+            skip = 0
+            while p1 >= 0:
+                if s[p1] == "#":
+                    skip += 1
+                else:
+                    if skip == 0:
+                        break
+                    skip -= 1
+                p1 -= 1
+
+            skip = 0
+            while p2 >= 0:
+                if t[p2] == "#":
+                    skip += 1
+                else:
+                    if skip == 0:
+                        break
+                    skip -= 1
+                p2 -= 1
+
+            if p1 >= 0 and p2 >= 0 and s[p1] != t[p2]:
+                return False
+            elif p1 < 0 and p2 < 0:
+                return True
+            elif p1 < 0 or p2 < 0:
+                return False
+            p1, p2 = p1-1, p2-1
+        return True
 
