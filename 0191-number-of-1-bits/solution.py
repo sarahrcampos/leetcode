596e480d96bc19aad68405d53725a85f3ca8d4bc
Sarah Campos <sarahrcampos@gmail.com>
2025-07-22 23:58:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (10.65%)

--- 0191-number-of-1-bits/solution.py
+++ 0191-number-of-1-bits/solution.py
@@ -2,6 +2,7 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
         while n > 0:
-            count += n%2
-            n = n // 2
+            if n & 1:
+                count += 1
+            n = n >> 1
         return count
