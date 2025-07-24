5090899d16cc467742de9d601d43c135a44cae1b
Sarah Campos <sarahrcampos@gmail.com>
2025-07-24 00:53:21 +0000

    [LeetCode Sync] Runtime - 44 ms (23.87%), Memory - 17.8 MB (29.10%)

--- 0190-reverse-bits/solution.py
+++ 0190-reverse-bits/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        power = 0
         res = 0
-        cur = n
-        while power < 32:
-            res += (cur%2) * 2**(31-power)
-            power += 1
-            cur = cur // 2
+        for i in range(32):
+            res += (2**i) * (0 if n & 2**31 == 0 else 1)
+            n = n << 1
         return res
