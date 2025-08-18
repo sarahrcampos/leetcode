15dce7ebd58f60e1bea3972b152b1517cd50614f
Sarah Campos <sarahrcampos@gmail.com>
2025-08-18 19:25:50 +0000

    [LeetCode Sync] Runtime - 2 ms (53.44%), Memory - 18.7 MB (61.18%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,10 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in map:
-                return [map[complement], i]
+            difference = target-nums[i]
+            if difference in map:
+                return [map[difference], i]
             map[nums[i]] = i
-
-        
+        return []
