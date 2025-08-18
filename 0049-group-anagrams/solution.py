b2f90fd3574a47fd0e57390bdfd143f02f0119a1
Sarah Campos <sarahrcampos@gmail.com>
2025-08-18 19:35:09 +0000

    [LeetCode Sync] Runtime - 15 ms (49.14%), Memory - 21.4 MB (39.59%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,31 +1,11 @@
-"""
-constraints
-1 <= strs.length <= 104
-0 <= strs[i].length <= 100
-strs[i] consists of lowercase English letters.
-"""
-
 class Solution:
-    #Time: O(n²logn)
-    #Space: O(n)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        map = {}
-        for word in strs:
-            key = "".join(sorted(word)) #O(nlogn)
-            if key not in map:
-                map[key] = [word]
+        result, map = [], {}
+        for s in strs:
+            sortS = str(sorted(s))
+            if sortS in map:
+                result[map[sortS]].append(s)
             else:
-                map[key].append(word)
-        return list(map.values())
-
-# class Solution:
-#     #Time: O(n²)
-#     #Space: O(n)
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         map = defaultdict(list)
-#         for word in strs: #O(n)
-#             frequencyMap = [0] * 26
-#             for c in word: #O(k)
-#                 frequencyMap[ord(c)%ord('a')] += 1
-#             map[tuple(frequencyMap)].append(word) #O(n)
-#         return list(map.values())
+                result.append([s])
+                map[sortS] = len(result)-1
+        return result
