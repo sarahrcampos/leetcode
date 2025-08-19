0298f72e20481019dffab6fce216eea1d804664a
Sarah Campos <sarahrcampos@gmail.com>
2025-08-19 23:34:24 +0000

    [LeetCode Sync] Runtime - 19 ms (65.28%), Memory - 18.1 MB (19.50%)

--- 0567-permutation-in-string/solution.py
+++ 0567-permutation-in-string/solution.py
@@ -1,18 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        letters2 = defaultdict(int)
+        windowSize = len(s1)
+        l = 0
+        countS, countWindow = defaultdict(int), defaultdict(int)
         for c in s1:
-            letters2[c] += 1
-        
-        l, r = 0, 0
-        letters1 = defaultdict(int)
-        while r < len(s2):
-            letters1[s2[r]] += 1
-            while letters1[s2[r]] > letters2[s2[r]]:
-                letters1[s2[l]] -= 1
-                l += 1
-            if r - l + 1 == len(s1):
-                return True
-            r += 1
-        return False
+            countS[c] += 1
 
+        for r in range(len(s2)):
+            countWindow[s2[r]] += 1
+            if r - l + 1 == windowSize:
+                # print(countWindow)
+                # print(countS)
+                if countWindow == countS:
+                    return True
+                countWindow[s2[l]] -= 1
+                if not countWindow[s2[l]]:
+                    del countWindow[s2[l]]
+                l += 1
+
+
+        return False
