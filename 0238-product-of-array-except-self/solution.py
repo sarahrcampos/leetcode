4aa66a6ac7d93b5c8b3c8096ea58cfa970e3de42
Sarah Campos <sarahrcampos@gmail.com>
2025-08-19 02:23:30 +0000

    [LeetCode Sync] Runtime - 39 ms (16.29%), Memory - 24.9 MB (36.22%)

--- 0238-product-of-array-except-self/solution.py
+++ 0238-product-of-array-except-self/solution.py
@@ -1,51 +1,25 @@
 class Solution:
-    #Space: O(1)
-    #Time: O(n)
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = [1]*n
-        #prefix
-        for i in range(1,n):
-            res[i] = res[i-1] * nums[i-1]
-        #suffix
-        nums.append(nums[n-1])
-        for i in range(n-1, 0, -1):
-            nums[i] = nums[i-1] * nums[i+1]
-       
-        #result
-        for i in range(n-2, -1, -1):
-            res[i] = res[i] * nums[i+2]
+        answer = [1] * (len(nums) + 1)
 
-        return res
+        for i in range(1, len(nums)):
+            answer[i] = nums[i-1] * answer[i-1]
         
-
-
-
-
-#1 - brute force - O(n²)
-    # def productExceptSelf(self, nums: List[int]) -> List[int]:
-    #     res = []
-    #     for i in range(len(nums)):
-    #         product = 1
-    #         for j in range(len(nums)):
-    #             if i == j: continue
-    #             product *= nums[j]
-    #         res.append(product)
+        for i in range(len(nums)-2, -1,-1):
+            nums[i] *= nums[i+1]
         
-    #     return res
+        answer[-1] = answer[-2]
+        print(answer)
+        for i in range(len(answer)-2, 0, -1):
+            answer[i] = answer[i-1] * nums[i]
+        
+        return answer[1:]
+        
+# [-1,1,0,-3,3]
+#prefix[i] = answer[i]
+# [1,-1,-1,0,0,1]
 
-#2 - prefix and suffix arrays
-    # def productExceptSelf(self, nums: List[int]) -> List[int]:
-    #     prefix, suffix = [1]*len(nums), [1]*len(nums)
-    #     for i in range(1,len(nums)):
-    #         prefix[i] = prefix[i-1] * nums[i-1]
-    #     for i in range(len(nums)-2, -1,-1):
-    #         suffix[i] = nums[i+1] * suffix[i+1]
-    #     #nums = [1,2,3,4]
-    #     #prefix = [1, 1, 2, 6]
-    #     #suffix = [24, 12, 4, 1]
+#suffix[i] = nums[i+1]
+# [0,0,0,-9,3]
 
-    #     res = []
-    #     for i in range(len(nums)):
-    #         res.append(prefix[i] * suffix[i])
-    #     return res
+#[1,0,0,9,0,0]
