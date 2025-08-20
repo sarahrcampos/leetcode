bbaf466891430102c78e8967dc5f4511f02202bc
Sarah Campos <sarahrcampos@gmail.com>
2025-08-20 14:23:12 +0000

    [LeetCode Sync] Runtime - 3 ms (84.20%), Memory - 21.8 MB (15.41%)

--- 0155-min-stack/solution.py
+++ 0155-min-stack/solution.py
@@ -1,42 +1,25 @@
-"""
-Constraints:
--231 <= val <= 231 - 1
-Methods pop, top and getMin operations will always be called on non-empty stacks.
-At most 3 * 104 calls will be made to push, pop, top, and getMin.
-
-Example
--2, 0, -3
-getMin = -3
--2,0
-top = 0
-getMin = -2
-
-"""
 class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min = float("inf")
 
     def push(self, val: int) -> None:
-        self.stack.append([val, self.min])
-        self.min = min(self.min, val)
+        self.stack.append([val, min(val, self.stack[-1][1]) if self.stack else val])
 
-    def pop(self) -> None:
-        if not self.stack:
-            return None
-        popped = self.stack.pop()
-        self.min = popped[1]
-        return popped[0]
+    def pop(self) -> None:  
+        if self.stack:
+            self.stack.pop()[0]
 
     def top(self) -> int:
-        return self.stack[-1][0]
-        
+        if self.stack:
+            return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.min
-        
+        if self.stack:
+            return self.stack[-1][1]
 
+# -2, 0, -3
+# -2, -2, -3
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
