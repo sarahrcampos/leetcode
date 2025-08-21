5326282cd7c342fe7d960cfc6b7a3bd49b5e3e5f
Sarah Campos <sarahrcampos@gmail.com>
2025-08-21 15:11:04 +0000

    [LeetCode Sync] Runtime - 166 ms (38.84%), Memory - 19.1 MB (28.64%)

--- 0907-koko-eating-bananas/solution.py
+++ 0907-koko-eating-bananas/solution.py
@@ -1,21 +1,28 @@
-class Solution(object):
-    def minEatingSpeed(self, piles, h):
-        MAX = max(piles)
-        left, right = 1, MAX
-        k = MAX
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)                
+        minimum = right
         while left <= right:
-            s = 0
-            middle = (left+right)//2
-            for bananas in piles:
-                s = s + bananas//middle
-                if bananas%middle != 0:
-                    s = s + 1
-            print("for: ", middle, middle, s)
-            if s > h:
-                left = middle + 1
-            else:
-                if middle < k: k = middle
+            middle = (right+left)//2
+            hours = 0
+            for pile in piles:
+                hours += (pile // middle) + (1 if pile % middle else 0)
+            if hours <= h:
                 right = middle - 1
-            print("while: ",left, right, k)                
-        return k
-        
+                minimum = min(minimum, middle)
+            else:
+                left = middle + 1            
+
+        return minimum
+
+
+
+
+
+#k = bananas/hora
+#if piles[i] < k -> piles[i] = 0
+#return k so she can eat all the bananas within h hours
+
+#piles = [3,6,7,11], h = 8
+#pra comer 1 pilha por hora -> 11 -> sobra 4 horas
+#
