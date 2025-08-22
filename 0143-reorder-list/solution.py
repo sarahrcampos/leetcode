9446c2086af5e3ef1aa8bc8c6a2ecbe52199b6b0
Sarah Campos <sarahrcampos@gmail.com>
2025-08-22 15:08:29 +0000

    [LeetCode Sync] Runtime - 4 ms (35.28%), Memory - 23.3 MB (81.08%)

--- 0143-reorder-list/solution.py
+++ 0143-reorder-list/solution.py
@@ -5,36 +5,28 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        s1, s2, f = head, head.next, head.next
-        head.next = None
-        while f and f.next:
-            f = f.next.next
-            temp = s2.next
-            s2.next = s1
-            s1 = s2
-            s2 = temp
-        #if not f -> odd -> end with s1 =>  s2 -> s1
-        #if f -> even -> end with s2 => s1 -> s2
-        if not s2:
+        slow, fast = head, head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow, fast = slow.next, fast.next.next
+        if not prev:
             return head
-        t1, t2 = s1.next, s2.next
-        if not f:
-            s2.next, s1.next = s1, None
-            s1 = t1
-            t1 = s1.next
-        else:
-            s2.next = None
-        
-        while s1:
-            s1.next = s2
-            s2 = t2
-            if t2:
-                temp = t2.next
-                t2.next = s1
-                t2 = temp
-            s1 = t1
-            if t1:
-                t1 = t1.next
-            
+        prev.next = None
+        r, slow.next = slow.next, None
+        while r:
+            temp, r = r, r.next
+            temp.next = slow
+            slow = temp
+
+        l, r = head, slow
+        while l and r:
+            tempL, tempR = l, r
+            l, r = l.next, r.next
+            tempL.next = tempR
+            tempR.next = l if l else r
         
         return head
+        
+            
+        
