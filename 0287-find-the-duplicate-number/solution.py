2646fd0933aed442645651618944a78335e745c3
Sarah Campos <sarahrcampos@gmail.com>
2025-08-22 18:36:40 +0000

    [LeetCode Sync] Runtime - 50 ms (19.75%), Memory - 29.1 MB (90.55%)

--- 0287-find-the-duplicate-number/solution.py
+++ 0287-find-the-duplicate-number/solution.py
@@ -1,18 +1,8 @@
-#array of integers nums containing n + 1 integers
-# each integer is in the range [1, n] inclusive
-#There is only one repeated number in nums, return this repeated number.
-#without extra space and without modyfing the array
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        #the array goes from 0 to n+1
-        #we use the indices as the elements
-        #if we already visited that index, that is the one repetead.
-        index = 0
-        while nums[index] > 0:
-            nums[index] = (-1) * nums[index]
-            index = (-1) * nums[index]
-        return index
-            
-    
-    # 0,   1,  2,  3, 4 -> index
-    # -1, -3, -4, -2, -2
+        i = 0
+        while i < len(nums):
+            if nums[i] < 0:
+                return i
+            nums[i] = -nums[i]
+            i = -nums[i]
