d351b5dd76a0a175a84c0c14b571a11b2e24c94c
Sarah Campos <sarahrcampos@gmail.com>
2025-08-23 22:50:02 +0000

    [LeetCode Sync] Runtime - 7 ms (45.76%), Memory - 20.9 MB (20.76%)

--- 0543-diameter-of-binary-tree/solution.py
+++ 0543-diameter-of-binary-tree/solution.py
@@ -11,10 +11,8 @@ class Solution:
             nonlocal maximum
             if not node:
                 return 0
-            left = dfs(node.left)
-            right = dfs(node.right)
+            left, right = dfs(node.left), dfs(node.right)
             maximum = max(maximum, left + right)
             return 1 + max(left, right)
-        
         dfs(root)
         return maximum
