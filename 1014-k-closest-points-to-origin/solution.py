9dfd92cde39bdc3cc3ce0a83b31a18b10afb625f
Sarah Campos <sarahrcampos@gmail.com>
2025-08-25 00:40:14 +0000

    [LeetCode Sync] Runtime - 84 ms (32.51%), Memory - 22.6 MB (66.68%)

--- 1014-k-closest-points-to-origin/solution.py
+++ 1014-k-closest-points-to-origin/solution.py
@@ -1,8 +1,13 @@
-class Solution(object):
-    #Space: O(2n)
-    #Time: O(klogn)
-    def kClosest(self, points, k):
-        heap = [ [x**2 + y**2, x, y] for x, y in points]
-        heapq.heapify(heap)
-        return [ heapq.heappop(heap)[1:3] for _ in range(k) ]
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        for x, y in points:
+            heapq.heappush(heap, [-(x**2+y**2), x, y])
+            if len(heap) > k:
+                heapq.heappop(heap)
         
+        res = []
+        while heap:
+            distance, x, y = heapq.heappop(heap)
+            res.append([x,y])
+        return res
