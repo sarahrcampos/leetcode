9790c233042bb8f02b2bcc2fce750f70f64bc706
Sarah Campos <sarahrcampos@gmail.com>
2025-08-25 01:15:59 +0000

    [LeetCode Sync] Runtime - 137 ms (49.20%), Memory - 19 MB (98.09%)

--- 0621-task-scheduler/solution.py
+++ 0621-task-scheduler/solution.py
@@ -1,24 +1,27 @@
 class Solution:
+    #Each CPU interval can be idle or allow the completion of one task
+    #there has to be a gap of at least n intervals between two tasks with the same label
     def leastInterval(self, tasks: List[str], n: int) -> int:
         counter = defaultdict(int)
         for task in tasks:
             counter[task] += 1
         heap = []
         for task, count in counter.items():
-            heapq.heappush(heap, [-count, task, 0])
-
-        time = 0
+            heap.append(-count)
+        heapq.heapify(heap)
         queue = deque()
-        while heap or queue:
-            while queue and queue[0][2] <= time:
-                heapq.heappush(heap, queue.popleft())
+        curr = 0
+        while queue or heap:
+            # print(curr)
+            # print(queue)
+            # print(heap)
+            while queue and queue[0][0] <= curr:
+                time, count = queue.popleft()
+                heapq.heappush(heap, count)
             if heap:
-                count, task, curTime = heapq.heappop(heap)
-                if count + 1 < 0:
-                    queue.append([count+1, task, time + n + 1])
-            time += 1
-
-        return time
-
-
-
+                count = heapq.heappop(heap) + 1
+                if count != 0:
+                    queue.append([curr + n + 1, count])
+            curr += 1
+        
+        return curr
