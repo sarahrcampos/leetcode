d87912ca76d545f6270bdf20fabffa82f14430d2
Sarah Campos <sarahrcampos@gmail.com>
2025-08-26 17:48:35 +0000

    [LeetCode Sync] Runtime - 5483 ms (18.12%), Memory - 17.8 MB (87.52%)

--- 0079-word-search/solution.py
+++ 0079-word-search/solution.py
@@ -1,25 +1,26 @@
-class Solution(object):
-    def exist(self, board, word):
-        ROWS, COLS = len(board), len(board[0])
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
-        used = set()
-        def backtrack(bi,bj,wi):
-            if wi == len(word):
-                return True
-            if bi<0 or bi>=ROWS or bj<0 or bj>=COLS or (bi,bj) in used or board[bi][bj] != word[wi]:
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        directions = [[-1,0],[1,0],[0,-1],[0,1]]
+        def backtrack(wi, visited, i, j):
+            if (i,j) in visited or board[i][j] != word[wi]:
                 return False
+            if wi == len(word)-1:
+                return True
             
-            used.add((bi,bj))
-            for di,dj in directions:
-                newi, newj = bi+di, bj+dj
-                if backtrack(newi, newj, wi+1):
-                    return True
-            used.remove((bi,bj))
+            
+            visited.add((i,j))
+            for di, dj in directions:
+                newI, newJ = i+di, j+dj
+                if 0 <= newI < len(board) and 0 <= newJ < len(board[0]) and (newI, newJ) not in visited:
+                    if backtrack(wi+1, visited, newI, newJ):
+                        return True
+            visited.remove((i,j))
+
             return False
-            
-        for i in range(ROWS):
-            for j in range(COLS):
-                if board[i][j] == word[0] and backtrack(i,j,0):
-                    return True
-        return False
         
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0] and backtrack(0,set(),i,j):
+                    return True
+
+        return False
