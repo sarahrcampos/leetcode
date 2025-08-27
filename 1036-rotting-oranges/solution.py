250bdc914cd2caa7295cebb7e148532eb373f382
Sarah Campos <sarahrcampos@gmail.com>
2025-08-27 21:07:38 +0000

    [LeetCode Sync] Runtime - 4 ms (47.90%), Memory - 17.9 MB (44.79%)

--- 1036-rotting-oranges/solution.py
+++ 1036-rotting-oranges/solution.py
@@ -1,34 +1,25 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        fresh, rotten = 0, 0
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+
+        fresh = 0
         queue = deque()
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 1:
                     fresh += 1
                 elif grid[i][j] == 2:
-                    rotten += 1
                     queue.append((i,j))
-        if not rotten and not fresh:
-            return 0
-
-        directions = [
-            [1,0],
-            [-1,0],
-            [0,-1],
-            [0,1]
-        ]
-        minutes = -1
+        time = 0
         while queue:
             for _ in range(len(queue)):
                 i, j = queue.popleft()
                 for di, dj in directions:
-                    newi, newj = i + di, j + dj
-                    if newi in range(0,ROWS) and newj in range(0,COLS) and grid[newi][newj] == 1:
+                    newI, newJ = i + di, j + dj
+                    if (0 <= newI < ROWS and 0 <= newJ < COLS and grid[newI][newJ] == 1):
+                        grid[newI][newJ] = 2
                         fresh -= 1
-                        grid[newi][newj] = 2
-                        queue.append((newi,newj))
-            minutes += 1
-        
-        return minutes if not fresh else -1
+                        queue.append((newI, newJ))
+            time += 1
+        return max(time-1, 0) if fresh == 0 else -1
