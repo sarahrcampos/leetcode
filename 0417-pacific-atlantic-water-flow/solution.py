c004709d75d8f1693cdda8c700be4cfbd630bf79
Sarah Campos <sarahrcampos@gmail.com>
2025-08-28 17:12:53 +0000

    [LeetCode Sync] Runtime - 31 ms (69.30%), Memory - 19.3 MB (80.55%)

--- 0417-pacific-atlantic-water-flow/solution.py
+++ 0417-pacific-atlantic-water-flow/solution.py
@@ -1,39 +1,35 @@
-#grid mxn
-#pacific -> left and top
-#atlantic -> right and bottom
-#4 directions -> only if the value is <=
-#output: lista de células que podem alcançar os dois oceanos
-#começar pelas beiradas 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pacific, atlantic = set(), set()
-        for i in range(ROWS):
-            pacific.add((i, 0))
-            atlantic.add((i, COLS-1))
-        for j in range(COLS):
-            pacific.add((0, j))
-            atlantic.add((ROWS-1, j))
-        
-        directions = [[0,1],[0,-1],[-1,0],[1,0]]
-        def bfs(elements):
-            visited = set()
-            queue = deque(elements)
+        ROWS, COLS = len(heights), len(heights[0])        
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        def bfs(queue, ocean):
             while queue:
-                for _ in range(len(queue)):
-                    i, j = queue.popleft()
-                    for di, dj in directions:
-                        newi, newj = i + di, j + dj
-                        if (newi in range(0,ROWS) and newj in range(0, COLS) and (newi, newj) not in elements and heights[newi][newj] >= heights[i][j]):
-                            queue.append((newi, newj))
-                            elements.add((newi, newj))
-        bfs(pacific)
-        bfs(atlantic)
+                i, j = queue.popleft()
+                ocean.add((i,j))
+                for di, dj in directions:
+                    newI, newJ = i + di, j + dj
+                    if (0 <= newI < ROWS and 
+                        0 <= newJ < COLS and 
+                        (newI, newJ) not in ocean and
+                        heights[newI][newJ] >= heights[i][j]):
+                        ocean.add((newI,newJ))
+                        queue.append((newI, newJ))
+            return ocean
+        
+        atlantic, pacific = deque(), deque()
+        for j in range(COLS): 
+            pacific.append((0, j))
+            atlantic.append((ROWS-1, j))
+        for i in range(ROWS):
+            pacific.append((i, 0))
+            atlantic.append((i, COLS-1))
+        
+        canReachAtlantic = bfs(atlantic, set())
+        canReachPacific = bfs(pacific, set())
+
         res = []
-        for i, j in pacific:
-            if (i, j) in atlantic:
-                res.append([i,j])
+        for i, j in canReachAtlantic:
+            if (i, j) in canReachPacific:
+                res.append([i, j])
         return res
-
-
 
