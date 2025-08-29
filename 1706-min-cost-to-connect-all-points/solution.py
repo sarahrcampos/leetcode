49eae17365cae97d3939337efd498be01921cf26
Sarah Campos <sarahrcampos@gmail.com>
2025-08-29 14:36:42 +0000

    [LeetCode Sync] Runtime - 3911 ms (8.58%), Memory - 97.8 MB (46.25%)

--- 1706-min-cost-to-connect-all-points/solution.py
+++ 1706-min-cost-to-connect-all-points/solution.py
@@ -1,22 +1,41 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+    
+    def find(self, node):
+        while node != self.parent[node]:
+            self.parent[node] = self.parent[self.parent[node]]
+            node = self.parent[node]
+        return node
+    
+    def union(self, node1, node2):
+        parent1, parent2 = self.find(node1), self.find(node2)
+        if parent1 == parent2:
+            return False
+        if self.rank[parent1] > self.rank[parent2]:
+            self.parent[parent2] = parent1
+            self.rank[parent1] += self.rank[parent2]
+        else:
+            self.parent[parent1] = parent2
+            self.rank[parent2] += self.rank[parent1]
+        return True
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        uf = UnionFind(len(points))
+        minHeap = []
         for i in range(len(points)):
+            xi, yi = points[i]
             for j in range(i+1, len(points)):
-                xi, yi = points[i]
                 xj, yj = points[j]
-                distance = abs(xi-xj) + abs(yi - yj)
-                graph[i].append((distance,j))
-                graph[j].append((distance,i))
-        heap = [(0, 0, 0)]
-        visited = set()
-        total = 0
-        while heap:
-            distance, source, node = heapq.heappop(heap)
-            if node in visited:
-                continue
-            visited.add(node)
-            total += distance
-            for weight, neighbor in graph[node]:
-                heapq.heappush(heap, (weight, node, neighbor))
-        return total
+                minHeap.append([abs(xi-xj)+abs(yi-yj), i, j])
+        heapq.heapify(minHeap)
+        res = 0
+        while minHeap:
+            distance, point1, point2 = heapq.heappop(minHeap)            
+            if uf.union(point1, point2):
+                res += distance
+        
+        return res
+
