7729f3633cdfac50e9e509c66a2f3768ed7abef1
Sarah Campos <sarahrcampos@gmail.com>
2025-09-02 00:17:32 +0000

    [LeetCode Sync] Runtime - 7 ms (89.01%), Memory - 20.2 MB (69.82%)

--- 0023-merge-k-sorted-lists/solution.py
+++ 0023-merge-k-sorted-lists/solution.py
@@ -4,26 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = ListNode(float("inf"))
-        curr = dummy
-        finished = set()
-        
-        while len(finished) < len(lists):
-            smallest = -1
-            for i in range(len(lists)):
-                if i in finished:
-                    continue
-                if not lists[i]:
-                    finished.add(i)
-                    continue
-                if smallest == -1 or lists[i].val < lists[smallest].val:
-                    smallest = i
-
-            if smallest != -1:
-                curr.next = lists[smallest]
-                lists[smallest] = lists[smallest].next       
-                curr = curr.next
-
-        curr.next = None
+    #merge two lists
+    def merge(self, list1, list2):
+        dummy = ListNode()
+        curr = dummy        
+        while list1 and list2:            
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1: curr.next = list1
+        if list2: curr.next = list2
         return dummy.next
+
+    #merge the lists in pairs
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        while len(lists) > 1:
+            temp = []
+            for i in range(0, len(lists) - 1, 2):
+                temp.append(self.merge(lists[i], lists[i+1]))
+            if len(lists) % 2:
+                temp.append(lists[len(lists) - 1])                    
+            lists = temp
+        return lists[0]
