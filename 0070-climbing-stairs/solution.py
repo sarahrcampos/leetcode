85cc7f887d12555b3f92b90c608f1ee59536c029
Sarah Campos <sarahrcampos@gmail.com>
2025-09-07 16:06:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (8.31%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -1,17 +1,14 @@
-class Solution(object):
-    #solution using dynamic programming
-    #Time: O(n)
-    #Space: O(n)
-    def climbStairs(self, n):
-        cache = {}
-        def climb(n2):
-            if n2 < 0:
-                return 0
-            if n2 == 0:
-                return 1
-            if n2 not in cache:
-                cache[n2] = climb(n2-1) + climb(n2-2)
-            return cache[n2]
-        return climb(n)
-                
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        first, second = 1, 1
+        n -= 2
+        while n >= 0:
+            temp = first
+            first += second
+            second = temp
+            n-=1
+        return first
         
+
+            
+
