ee9afca04e01670c3e3923e26761b46631ad085e
Sarah Campos <sarahrcampos@gmail.com>
2025-09-07 20:22:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (59.20%)

--- 0213-house-robber-ii/solution.py
+++ 0213-house-robber-ii/solution.py
@@ -1,17 +1,20 @@
+#[0] and [-1] cannot be both in the answer
+#lets just try force them in the answer and see which one does better
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
+        withZero = [0] * n
+        withZero[0], withZero[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n-1):
+            withZero[i] = max(withZero[i-1], nums[i] + withZero[i - 2])
         
-        #A[i] = max(A[i-1], A[i-2] + wi)
-        def robSubset(subset):
-            last, second2last = 0, 0
-            for n in subset:
-                temp = max(last, second2last + n)
-                second2last = last
-                last = temp #meu max atual, que vai virar o último do próximo n
-            return last
+        #force [-1]
+        nums[n-2] = max(nums[n-2], nums[n-1])
+        for i in range(len(nums)-3, 0, -1):
+            nums[i] = max(nums[i+1], nums[i] + nums[i+2])
         
-        return max(robSubset(nums[1:]), robSubset(nums[:len(nums)-1]))
+        return max(withZero[-2], nums[1])
         
 
