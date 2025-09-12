18eab09a2e719ac12d6588e017d035ad58bfb7b1
Sarah Campos <sarahrcampos@gmail.com>
2025-09-12 18:39:40 +0000

    [LeetCode Sync] Runtime - 676 ms (31.41%), Memory - 25.5 MB (87.48%)

--- 1250-longest-common-subsequence/solution.py
+++ 1250-longest-common-subsequence/solution.py
@@ -1,13 +1,10 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        m, n = len(text1), len(text2)
-        memo = [0] * (m +1)
-        for i in range(n):
-            current = [0]*(m+1)
-            for j in range(m):
-                if text1[j] == text2[i]:
-                    current[j+1] = memo[j] + 1
-                else:
-                    current[j+1] = max(current[j], memo[j+1])
-            memo = current
-        return memo[-1]
+        ROWS, COLS = len(text1), len(text2)
+        memo = [[0]*(COLS+1) for _ in range(ROWS+1)]
+
+        for i in range(1, ROWS+1):
+            for j in range(1, COLS+1):
+                memo[i][j] = max(memo[i-1][j], memo[i][j-1], memo[i-1][j-1] + (1 if text1[i-1] == text2[j-1] else 0))
+        #print(memo)
+        return memo[ROWS][COLS]
