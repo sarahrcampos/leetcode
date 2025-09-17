7ca6746e519624ebbe3f1f56387a92e5f4d7c20a
Sarah Campos <sarahrcampos@gmail.com>
2025-09-17 01:11:11 +0000

    [LeetCode Sync] Runtime - 19 ms (67.71%), Memory - 18.8 MB (11.88%)

--- 0055-jump-game/solution.py
+++ 0055-jump-game/solution.py
@@ -1,22 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        cache = {}
-
-        def backtrack(i):
-            if i in cache:
-                return cache[i]
-            if i == len(nums)-1:
-                cache[i] = True
-                return True
-            if i >= len(nums) or nums[i] == 0:
-                cache[i] = False
-                return False           
-            
-            for j in range(nums[i],0,-1):
-                if backtrack(i + j):
-                    cache[i] = True
-                    return True
-            cache[i] = False
-            return False
-        return backtrack(0)
-        
+        nextTrue = len(nums) - 1
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i] >= nextTrue: #can reach
+                nextTrue = i
+        return nextTrue == 0
