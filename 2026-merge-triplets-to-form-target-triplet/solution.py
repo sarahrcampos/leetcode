09419ff304c64b66808822017030ac72375b8509
Sarah Campos <sarahrcampos@gmail.com>
2025-09-18 20:22:17 +0000

    [LeetCode Sync] Runtime - 11 ms (79.27%), Memory - 55.4 MB (21.28%)

--- 2026-merge-triplets-to-form-target-triplet/solution.py
+++ 2026-merge-triplets-to-form-target-triplet/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        count0, count1, count2 = 0, 0, 0
-        for triplet in triplets:
-            if triplet[0] == target[0] and triplet[1] <= target[1] and triplet[2] <= target[2]:
-                count0 = 1
-            if triplet[1] == target[1] and triplet[0] <= target[0] and triplet[2] <= target[2]:
-                count1 = 1
-            if triplet[2] == target[2] and triplet[0] <=target[0] and triplet[1] <= target[1]:
-                count2 = 1
-            if count0 and count1 and count2:
+        found0 = found1 = found2 = False 
+        for triplet in triplets:            
+            found0 |= (triplet[0] == target[0] and triplet[1] <= target[1] and triplet[2] <= target[2])                
+            found1 |= (triplet[1] == target[1] and triplet[0] <= target[0] and triplet[2] <= target[2])                
+            found2 |= (triplet[2] == target[2] and triplet[0] <= target[0] and triplet[1] <= target[1])                
+            if found0 and found1 and found2:
                 return True
         return False
+            
