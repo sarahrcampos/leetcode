383a3283843a25270eab32d3f58bbba861ce9473
Sarah Campos <sarahrcampos@gmail.com>
2025-09-19 01:30:45 +0000

    [LeetCode Sync] Runtime - 4 ms (19.98%), Memory - 19.7 MB (60.30%)

--- 0057-insert-interval/solution.py
+++ 0057-insert-interval/solution.py
@@ -1,18 +1,23 @@
+#intervals = [ [1,3],insert here,[6,9]], newInterval = [2,5]
+#[-1,0]
+#[4,5]
+#[10,12]
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res= []
-        inserted = False
-        for lowerBound, upperBound in intervals:
-            if upperBound < newInterval[0]:
-                res.append([lowerBound, upperBound])
-            elif newInterval[1] < lowerBound:
-                if not inserted:
-                    res.append(newInterval)
-                    inserted = True
-                res.append([lowerBound, upperBound])
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:        
+        newList = []
+        for i in range(len(intervals)):
+            interval = intervals[i]
+            if newInterval[1] < interval[0]:
+                return newList + [newInterval] + intervals[i:]
+            
+            if newInterval[0] <= interval[1]:
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
             else:
-                newInterval = [min(lowerBound, newInterval[0]), max(upperBound, newInterval[1])]
-        if not inserted:
-            res.append(newInterval)
-
-        return res
+                newList.append(interval)
+        newList.append(newInterval)
+        return newList 
+            
+#intervals = [[1,3],[6,9]], newInterval = [2,5]
+#newInterval = [1, 5]
+#
