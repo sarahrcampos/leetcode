3b41a47eaf14dc988e9071927ce0fd3470ccafb2
Sarah Campos <sarahrcampos@gmail.com>
2025-09-19 02:02:50 +0000

    [LeetCode Sync] Runtime - 10 ms (34.99%), Memory - 21 MB (63.66%)

--- 0056-merge-intervals/solution.py
+++ 0056-merge-intervals/solution.py
@@ -1,17 +1,20 @@
+#[[-1,0], [1,3],[2,6],[8,10],[11,14], [15,18]]
+#[14,17] [8,10]
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        res = []
-        current = intervals[0]
-        
+        newList = []
+        newInterval = intervals[0]
         for i in range(1, len(intervals)):
-            if current[1] < intervals[i][0]: #current upperbound is less than intervals[i] lowerbound, so current is not in this interval
-                res.append(current[::])
-                current = intervals[i]
-            elif intervals[i][1] < current[0]:
-                res.append(intervals[i][::])
+            interval = intervals[i]
+            if newInterval[1] < interval[0]:
+                newList.append(newInterval)
+                newInterval = interval
+                continue
+            if newInterval[0] <= interval[1]:
+                newInterval[0] = min(interval[0], newInterval[0])                
+                newInterval[1] = max(interval[1], newInterval[1])
             else:
-                current[0] = min(current[0], intervals[i][0])
-                current[1] = max(current[1], intervals[i][1])
-        res.append(current)
-        return res
+                newList.append(interval)
+        newList.append(newInterval)
+        return newList
