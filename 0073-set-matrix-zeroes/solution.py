977c3894e11eb0c950fa61056a3602f9725aed37
Sarah Campos <sarahrcampos@gmail.com>
2025-09-22 01:40:53 +0000

    [LeetCode Sync] Runtime - 4 ms (36.83%), Memory - 18.5 MB (82.36%)

--- 0073-set-matrix-zeroes/solution.py
+++ 0073-set-matrix-zeroes/solution.py
@@ -1,20 +1,39 @@
-class Solution(object):
-    #Time: O(n)
-    #Space: O(n)
-    def setZeroes(self, matrix):
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         ROWS, COLS = len(matrix), len(matrix[0])
-        zero_rows = set()
-        zero_cols = set()
+        rows, cols = set(), set()
         for i in range(ROWS):
             for j in range(COLS):
                 if matrix[i][j] == 0:
-                    zero_rows.add(i)
-                    zero_cols.add(j)
-        for i in range(ROWS):
-            for j in range(COLS):
-                if i in zero_rows or j in zero_cols:
-                    matrix[i][j] = 0
+                    rows.add(i)
+                    cols.add(j)
+        for row in rows:
+            matrix[row] = [0] * COLS
+        for col in cols:
+            for i in range(ROWS):
+                matrix[i][col] = 0
+
+        
+        
+        #O(n³):
+        # def setInf(row, col):
+        #     for i in range(ROWS):
+        #         if matrix[i][col] != 0 and matrix[i][col] != float("inf"):
+        #             matrix[i][col] = float("inf")
+        #     for j in range(COLS):
+        #         if matrix[row][j] != 0 and matrix[row][j] != float("inf"):
+        #             matrix[row][j] = float("inf")
+        
+        # for i in range(ROWS):
+        #     for j in range(COLS):
+        #         if matrix[i][j] == 0:
+        #             setInf(i, j)
+        # for i in range(ROWS):
+        #     for j in range(COLS):
+        #         if matrix[i][j] == float("inf"):
+        #             matrix[i][j] = 0
+
+
 
     
-
         
