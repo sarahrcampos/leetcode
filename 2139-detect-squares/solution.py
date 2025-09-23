9d1178511822234b7c14a17c20d6767b838fb34f
Sarah Campos <sarahrcampos@gmail.com>
2025-09-23 14:55:21 +0000

    [LeetCode Sync] Runtime - 82 ms (70.58%), Memory - 19.5 MB (80.15%)

---
+++ 2139-detect-squares/solution.py
@@ -0,0 +1,26 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.pts = defaultdict(int)
+
+    def add(self, point: List[int]) -> None:
+        self.pts[tuple(point)] += 1
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+        px, py = point
+        print('New')
+        for x, y in self.pts:
+            if (abs(px - x) != abs(py - y)) or x == px or y == py:
+                continue
+            if (px, y) in self.pts and (x, py) in self.pts:
+                # Multiply with the number of occurrences of the repeated point represented by self.pts[(x, y)]
+                res += self.pts[(px, y)] * self.pts[(x, py)] * self.pts[(x, y)]
+        return res
+        
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
