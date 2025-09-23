501921e0761016c919e257d65b1482314fdff843
Sarah Campos <sarahrcampos@gmail.com>
2025-09-23 18:50:22 +0000

    [LeetCode Sync] Runtime - 31 ms (15.20%), Memory - 18.8 MB (83.92%)

--- 0268-missing-number/solution.py
+++ 0268-missing-number/solution.py
@@ -1,5 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        expected = sum(range(0,len(nums)+1))
-        calculated = sum(nums)
-        return expected - calculated
+        n = len(nums)
+        count = 0
+        for num in nums:
+            count |= 1 << num
+        for i in range(n+1):
+            iFlag = count & 1
+            if not iFlag:
+                return i
+            count = count >> 1
+        
