65ed64ab5e711792bd810ddea12008ac74c66b65
Sarah Campos <sarahrcampos@gmail.com>
2025-09-23 18:21:50 +0000

    [LeetCode Sync] Runtime - 15 ms (30.34%), Memory - 18.6 MB (83.07%)

--- 0338-counting-bits/solution.py
+++ 0338-counting-bits/solution.py
@@ -1,12 +1,24 @@
+import math
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0]
+        res = [0]
+        currLog, head = 0, 0
         for i in range(1, n+1):
-            cur = i
-            count = 0
-            while cur:
-                cur &= (cur-1)
-                count +=1
-            ans.append(count)
-        return ans
+            if int(math.log(i, 2)) > currLog:
+                currLog, head = int(math.log(i, 2)), i
+                res.append(1)
+                continue
+            res.append(res[head] + (res[i-head] if head > 0 else 1))
+        return res
 
+        #O(nlogn):
+        # res = []
+        # for i in range(n+1):
+        #     num = i
+        #     count = 0
+        #     while num > 0:
+        #         count += num & 1
+        #         num = num >> 1
+        #     res.append(count)
+        # return res
+                
