c2879c3c13f354237ff982882dfd37480e84f389
Sarah Campos <sarahrcampos@gmail.com>
2025-09-26 00:52:27 +0000

    [LeetCode Sync] Runtime - 560 ms (74.17%), Memory - 20.7 MB (85.44%)

--- 0015-3sum/solution.py
+++ 0015-3sum/solution.py
@@ -1,29 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort() #nlogn
-        def twoSum(start, end, target):
-            res = []
-            l, r = start, end
-            while l < r:
-                if nums[l] + nums[r] == target:
-                    res.append([-target, nums[l], nums[r]])
-                    l = l + 1
-                    while nums[l] == nums[l-1] and l < r:
-                        l += 1
-                elif nums[l] + nums[r] > target:
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i-1 >= 0 and nums[i] == nums[i-1]: #skip duplicates to avoid duplicated triplets
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:                
+                total = nums[i] + nums[l] + nums[r]
+                if total > 0:
                     r -= 1
-                else:
+                elif total < 0:
                     l += 1
-            return res
-
-        answer = []
-        i = 0
-        while i < len(nums):
-            answer += twoSum(i+1, len(nums)-1, -nums[i])
-            i += 1
-            while i < len(nums) and nums[i] == nums[i-1]:
-                i += 1        
-        return answer
-
-
-#[-4, -3, -2, -1, -1, 0, 0, 1, 2, 3, 4]
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1 #escolher arbitrariamente mover o l, pq se for a decisão errada vai se ajeitar na próxima iteração
+                    while l < len(nums) and nums[l] == nums[l-1]:
+                        l += 1      
+        return res
