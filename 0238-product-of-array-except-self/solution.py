d9bfa8addb329be497dd9658deb2a5447193b4cc
Sarah Campos <sarahrcampos@gmail.com>
2026-02-23 20:33:06 +0000

    [LeetCode Sync] Runtime - 23 ms (57.89%), Memory - 25.4 MB (70.11%)

--- 0238-product-of-array-except-self/solution.py
+++ 0238-product-of-array-except-self/solution.py
@@ -1,25 +1,12 @@
+#res = [24,12,8,6]
+#post = 24
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        answer = [1] * (len(nums) + 1)
-
+        res = [1] * len(nums)
         for i in range(1, len(nums)):
-            answer[i] = nums[i-1] * answer[i-1]
-        
-        for i in range(len(nums)-2, -1,-1):
-            nums[i] *= nums[i+1]
-        
-        answer[-1] = answer[-2]
-        print(answer)
-        for i in range(len(answer)-2, 0, -1):
-            answer[i] = answer[i-1] * nums[i]
-        
-        return answer[1:]
-        
-# [-1,1,0,-3,3]
-#prefix[i] = answer[i]
-# [1,-1,-1,0,0,1]
-
-#suffix[i] = nums[i+1]
-# [0,0,0,-9,3]
-
-#[1,0,0,9,0,0]
+            res[i] = res[i-1] * nums[i-1]
+        post = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] = res[i] * post
+            post *= nums[i]
+        return res
