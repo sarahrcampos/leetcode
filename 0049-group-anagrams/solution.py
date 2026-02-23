36d20e32057eb330e31c12981ecea2f259f2fc94
Sarah Campos <sarahrcampos@gmail.com>
2026-02-23 19:44:23 +0000

    [LeetCode Sync] Runtime - 13 ms (54.46%), Memory - 22.1 MB (67.23%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,11 +1,9 @@
+#1 - keep a hashmap, the key is a word sorted -> n*(klogk) where k = max length of a word (100)
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result, map = [], {}
-        for s in strs:
-            sortS = str(sorted(s))
-            if sortS in map:
-                result[map[sortS]].append(s)
-            else:
-                result.append([s])
-                map[sortS] = len(result)-1
-        return result
+        groups = defaultdict(list)
+        for word in strs:
+            key = "".join(sorted(word))
+            groups[key].append(word)
+        return list(groups.values())
+        
